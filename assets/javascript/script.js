dde7f6065808fddaf6386fcd076636b1699f78b9
kbetlach <kbetlach@gmail.com>
2020-05-28 13:31:22 -0500

    finished focus curation. started javascript. audio plays

---
+++ assets/javascript/script.js
@@ -0,0 +1,25 @@
+function playFocus() {
+    let ophelia = new Audio("assets/audio/focus/ophelia.mp3");
+    let bluebell = new Audio("assets/audio/focus/bluebell.mp3");
+    let horses = new Audio("assets/audio/focus/horses.mp3");
+    let sierraTracks = new Audio("assets/audio/focus/sierra_tracks.mp3");
+    let heyComeTogether = new Audio("assets/audio/focus/hey_come_together.mp3");
+
+    horses.play();
+}
+
+function playMeditative() {
+
+}
+
+function playUnplugged() {
+
+}
+
+function playNightDrive() {
+
+}
+
+function playStudying() {
+
+}
