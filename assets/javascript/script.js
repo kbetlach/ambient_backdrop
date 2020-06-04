9c3ac19f97c2a5dac5c5c92a38e3af080e77e49a
kbetlach <kbetlach@gmail.com>
2020-06-04 14:47:26 -0500

    converted js to a jquery playlist template. added howler.js dependency. html audio button is not an audio controller

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,35 +1,25 @@
-let audioPlaying = false;
-console.log(audioPlaying);
+  function audioPlayer(){
+        var currentSong = 0;
+            $("#audioPlayer")[0].src = $("#playlist li a")[0];
+            $("#audioPlayer")[0].play();
+            $("#playlist li a").click(function(e){
+               e.preventDefault(); 
+               $("#audioPlayer")[0].src = this;
+               $("#audioPlayer")[0].play();
+               $("#playlist li").removeClass("current-song");
+                currentSong = $(this).parent().index();
+                $(this).parent().addClass("current-song");
+            });
+            
+        $("#audioPlayer")[0].addEventListener("ended", function(){
+            currentSong++;
+                if(currentSong == $("#playlist li a").length)
+                currentSong = 0;
+            $("#playlist li").removeClass("current-song");
+            $("#playlist li:eq("+currentSong+")").addClass("current-song");
+            $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
+            $("#audioPlayer")[0].play();
+    });
+}
 
-    let ophelia = new Audio("assets/audio/focus/ophelia.mp3");
-    let bluebell = new Audio("assets/audio/focus/bluebell.mp3");
-    let horses = new Audio("assets/audio/focus/horses.mp3");
-    let heyComeTogether = new Audio("assets/audio/focus/hey_come_together.mp3");
-    let england = new Audio("assets/audio/focus/england.mp3");
-    let memories = new Audio("assets/audio/focus/living_in_my_memories.mp3")
-    let brokenThings = new Audio("assets/audio/meditative/broken_things.mp3");
-    let canada = new Audio("assets/audio/meditative/canada.mp3");
-    let lazyMorning = new Audio("assets/audio/meditative/lazy_morning.mp3");
-    let sleepwalker = new Audio("assets/audio/meditative/sleepwalker_III.mp3");
-    let theHealing = new Audio("assets/audio/meditative/the_healing.mp3");
-    let angelina = new Audio("assets/audio/studying/angelina.mp3");
-    let memoriesRenewed = new Audio("assets/audio/studying/memories_renewed.mp3");
-    let candyIsland = new Audio("assets/audio/studying/candy_island.mp3");
-    let coffee = new Audio("assets/audio/studying/coffee.mp3");
-    let warm = new Audio("assets/audio/studying/warm.mp3");
-
-    let songs = [ophelia, bluebell, horses, heyComeTogether, england, memories, brokenThings, canada, lazyMorning, sleepwalker, theHealing, angelina, memoriesRenewed, candyIsland, coffee, warm];
-
-    function play() {
-
-        audioplaying = true;
-        let randomSong = songs[Math.floor(Math.random() * songs.length)];
-
-        if (audioplaying === true) {
-            randomSong.pause();
-            randomSong.currentTime = 0;
-        }
-
-        randomSong.play();
-        console.log(randomSong);    
-    }
+audioPlayer();
